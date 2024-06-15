b7918a0b5b38e98b3b84f528cc802b6664181ad6
tsunyoku <tsunyoku@gmail.com>
2024-06-15 13:59:03 +0100

    implement `Mod` for `ModClassic`

--- Sources/Shared/Stubs/osu.Game/Rulesets/Mods/ModClassic.cs
+++ Sources/Shared/Stubs/osu.Game/Rulesets/Mods/ModClassic.cs
@@ -1,6 +1,21 @@
-﻿namespace osu.Game.Rulesets.Mods;
+﻿using osu.Framework.Graphics.Sprites;
+using osu.Framework.Localisation;
 
-public class ModClassic : Mod
+namespace osu.Game.Rulesets.Mods;
+
+public abstract class ModClassic : Mod
 {
-    
+    public override string Name => "Classic";
+
+    public override string Acronym => "CL";
+
+    public override double ScoreMultiplier => 0.96;
+
+    public override IconUsage? Icon => FontAwesome.Solid.History;
+
+    public override LocalisableString Description => "Feeling nostalgic?";
+
+    public override ModType Type => ModType.Conversion;
+
+    public sealed override bool Ranked => false;
 }
